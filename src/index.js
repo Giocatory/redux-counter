fd20f8c43e86a58407bf92a2005261a3cc33c1af
Giocatory <nivolyas@gmail.com>
2023-05-08 23:12:15 +0800

    creating first config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {legacy_createStore as createStore} from 'redux';
+import {Provider} from'react-redux';
+import counter from "./reducer";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(counter);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
